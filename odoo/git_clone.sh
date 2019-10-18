236c12361443e40557fd3e71aacba1dc851c6aa4
Ubuntu <ubuntu@ip-172-31-31-144.us-east-2.compute.internal>
2019-10-18 22:30:39 +0000

    [FIX] install git and ports

--- odoo/git_clone.sh
+++ odoo/git_clone.sh
@@ -1,14 +1,14 @@
 # file run:sh
 #!/bin/sh
 
-pre = "https://"
-token = $GIT_TOKEN
-branch = $GIT_BRANCH
-odoo_addons = "@github.com/nahualventure/odoo-addons"
-odoo_addons_external = "@github.com/nahualventure/odoo-addons-external"
+pre="https://"
+token=$GIT_TOKEN
+branch=$GIT_BRANCH
+odoo_addons="@github.com/nahualventure/odoo-addons"
+odoo_addons_external="@github.com/nahualventure/odoo-addons-external"
 
-addons_url = $pre$token$odoo_addons
-git clone -b $branch $odoo_addons
+addons_url=$pre$token$odoo_addons
+git clone -b $branch $addons_url
 
-addons_external_url = $pre$token$odoo_addons_external
-git clone -b $branch $odoo_addons_external
+addons_external_url=$pre$token$odoo_addons_external
+git clone -b $branch $addons_external_url
