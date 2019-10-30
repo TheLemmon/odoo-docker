794ceb6555dca95233814e11bdaf8b690c93ad85
Thelemmon <luisfer5624@gmail.com>
2019-10-29 20:21:58 -0600

    second version

--- odoo/git_clone.sh
+++ odoo/git_clone.sh
@@ -1,14 +1,18 @@
 # file run:sh
 #!/bin/sh
 
-pre = "https://"
-token = $GIT_TOKEN
-branch = $GIT_BRANCH
-odoo_addons = "@github.com/nahualventure/odoo-addons"
-odoo_addons_external = "@github.com/nahualventure/odoo-addons-external"
+pre="https://"
+token=$GIT_TOKEN
+branch=$GIT_BRANCH
+odoo_addons="@github.com/nahualventure/odoo-addons"
+odoo_addons_external="@github.com/nahualventure/odoo-addons-external"
+odoo_enterprise="@github.com/odoo/enterprise.git"
 
-addons_url = $pre$token$odoo_addons
-git clone -b $branch $odoo_addons
+addons_url=$pre$token$odoo_addons
+git clone -b $branch $addons_url
 
-addons_external_url = $pre$token$odoo_addons_external
-git clone -b $branch $odoo_addons_external
+addons_external_url=$pre$token$odoo_addons_external
+git clone -b $branch $addons_external_url
+
+enterprise_url=$pre$token$odoo_enterprise
+git clone -b $branch $enterprise_url
