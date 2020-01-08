c0cf208e9714a8b46d3856730df52f5bb73f069f
Thelemmon <luisfer5624@gmail.com>
2020-01-08 09:54:58 -0600

    [ADD] support for external repositories

---
+++ odoo/config/restructure-addons-external.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+import shutil
+
+yml_path = "home/odoo/config/addons-external.yml"
+src_path = "/home/odoo/src/addons-external"
+main_path = "/home/odoo/temp/addons-external"
+
+if __name__ == "__main__":
+    with open(path) as file:
+        doc = yaml.full_load(file)
+        paths_to_move = doc.keys()
+        
+        for (root, dirs, files) in os.walk(main_path, topdown=True):
+            current_path = f"./{root.split('/')[-1]}"        
+            if current_path not in paths_to_move:
+                continue
+            
+            for dir_name in dirs:
+                shutil.move(f"{root}/{dir_name}", src_path, copy_function=shutil.copytree)
