b1a724b65f232ca1d7dd6e42501c48993f1ba2af
Thelemmon <luisfer5624@gmail.com>
2020-01-09 10:36:53 -0600

    [FIX] remove recursion from script

--- odoo/config/restructure-addons-external.py
+++ odoo/config/restructure-addons-external.py
@@ -5,23 +5,13 @@ import shutil
 
 YML_PATH = "/home/odoo/config/addons-external.yml"
 DESTINATION = "/home/odoo/src/addons-external/"
-MAIN_PATH = "/home/odoo/temp/addons-external"
+SOURCE_PATH = "/home/odoo/temp/addons-external/"
 GITHUB_DIRECTORIES = ('branches', 'hooks', 'info', 'logs', 'objects', 'refs', '.git')
 
 
 if __name__ == "__main__":
-    with open(YML_PATH) as file:
-        doc = yaml.safe_load(file)
-        paths_to_move = doc.keys()
-        
-        for (root, dirs, files) in os.walk(MAIN_PATH, topdown=True):
-            current_path = f"./{root.split('/')[-1]}"
-            if current_path not in paths_to_move:
+    for (root, dirs, files) in os.walk(SOURCE_PATH, topdown=True):
+        for dir_name in dirs:
+            if dir_name in GITHUB_DIRECTORIES:
                 continue
-            
-            for dir_name in dirs:
-                if dir_name in GITHUB_DIRECTORIES:
-                    continue
-                source = f"{root}/{dir_name}"
-                print(source, DESTINATION)
-                shutil.move(source, DESTINATION)
+            shutil.move(f"{root}/{dir_name}", DESTINATION)
