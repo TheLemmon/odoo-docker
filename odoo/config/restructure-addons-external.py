7bda7f3618ef1400216a7cb69016b10f528e6722
Thelemmon <luisfer5624@gmail.com>
2020-01-09 12:39:24 -0600

    [ADD] specific modules from repository

--- odoo/config/restructure-addons-external.py
+++ odoo/config/restructure-addons-external.py
@@ -5,12 +5,24 @@ import shutil
 
 YML_PATH = "/home/odoo/config/addons-external.yml"
 DESTINATION = "/home/odoo/src/addons-external/"
-SOURCE_PATH = "/home/odoo/temp/addons-external/"
-GITHUB_DIRECTORIES = ('branches', 'hooks', 'info', 'logs', 'objects', 'refs', '.git', 'README.md')
+SOURCE = "/home/odoo/temp/addons-external"
+GITHUB_DIRECTORIES = ('branches', 'hooks', 'info', 'logs', 'objects', 'refs', '.git')
+
 
 if __name__ == "__main__":
-    for (root, dirs, files) in os.walk(SOURCE_PATH, topdown=True):
-        for dir_name in dirs:
-            if dir_name in GITHUB_DIRECTORIES:
-                continue
-            shutil.move(f"{root}/{dir_name}", DESTINATION)
+    with open(YML_PATH) as file:
+        doc = yaml.full_load(file)
+        for repository, attributes in doc.items():
+            dir_name = repository.split('/')[-1]
+            for (root, dirs, files) in os.walk(f"{SOURCE}/{dir_name}"):
+                modules = attributes.get('modules', dirs)
+                if isinstance(modules, str):
+                    modules = modules.split(',')
+                for dir_name in modules:
+                    if dir_name in GITHUB_DIRECTORIES:
+                        continue
+                    source = f"{root}/{dir_name}"
+                    if path.exists(source):
+                        shutil.move(source, DESTINATION)
+                break
+        shutil.rmtree(SOURCE)
