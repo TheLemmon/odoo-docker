0c4ed9198e3d2edaae7844da09e846441e4e4be2
Thelemmon <luisfer5624@gmail.com>
2020-01-09 10:10:06 -0600

    c

--- odoo/config/restructure-addons-external.py
+++ odoo/config/restructure-addons-external.py
@@ -4,7 +4,7 @@ import yaml
 import shutil
 
 YML_PATH = "/home/odoo/config/addons-external.yml"
-SRC_PATH = "/home/odoo/src/addons-external"
+DESTINATION = "/home/odoo/src/addons-external/"
 MAIN_PATH = "/home/odoo/temp/addons-external"
 GITHUB_DIRECTORIES = ('branches', 'hooks', 'info', 'logs', 'objects', 'refs', '.git')
 
@@ -15,13 +15,13 @@ if __name__ == "__main__":
         paths_to_move = doc.keys()
         
         for (root, dirs, files) in os.walk(MAIN_PATH, topdown=True):
-            current_path = f"./{root.split('/')[-1]}"        
+            current_path = f"./{root.split('/')[-1]}"
             if current_path not in paths_to_move:
                 continue
             
             for dir_name in dirs:
-                print(dir_name)
                 if dir_name in GITHUB_DIRECTORIES:
                     continue
-                print(f"{root}/{dir_name}")
-                shutil.move(f"{root}/{dir_name}", SRC_PATH)
+                source = f"{root}/{dir_name}"
+                print(source, DESTINATION)
+                shutil.move(source, DESTINATION)
